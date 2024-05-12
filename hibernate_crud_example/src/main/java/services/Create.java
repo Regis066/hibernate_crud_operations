9cbf54ff7ddbc826e1fa39293dbc45f167bfbf5e
Mugisha Regis <mugisharegis72@gmail.com>
2024-05-12 19:52:21 +0200

    test lazy/eager fetch types

--- hibernate_crud_example/src/main/java/services/Create.java
+++ hibernate_crud_example/src/main/java/services/Create.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import conf.SessionFactoryProvider;
+import models.Course;
 import models.Student;
 
 public class Create {
@@ -15,7 +16,15 @@ public class Create {
 		Session session = sessionFactory.openSession();
 		Transaction t = session.beginTransaction();
 
-		Student st1 = new Student("John", 10);
+		Student st1 = new Student("Regis", 15);
+
+		Course c1 = new Course("Java", st1);
+		Course c2 = new Course("Math", st1);
+		Course c3 = new Course("Science", st1);
+
+		st1.getCourses().add(c1);
+		st1.getCourses().add(c2);
+		st1.getCourses().add(c3);
 
 		session.persist(st1);
 		t.commit();
