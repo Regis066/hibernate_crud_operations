03c2db4b15fc1322efb9f5e4600918bc31b9ba4a
Mugisha Regis <mugisharegis72@gmail.com>
2024-05-12 19:46:52 +0200

    feat(model): add Course model

---
+++ hibernate_crud_example/src/main/java/models/Course.java
@@ -0,0 +1,57 @@
+package models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "course_id")
+	private int id;
+
+	@Column(name = "course_name")
+	private String name;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "student_id")
+	private Student student;
+
+	public Course() {
+
+	}
+
+	public Course(String name) {
+		this.name = name;
+	}
+
+	public Course(String name, Student student) {
+		this.name = name;
+		this.student = student;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+}
