0fd528baa425e203364e300b3a29e711670c4dec
Mugisha Regis <mugisharegis72@gmail.com>
2024-05-12 19:48:33 +0200

    update: add Course as another annotated class

--- hibernate_crud_example/src/main/java/conf/SessionFactoryProvider.java
+++ hibernate_crud_example/src/main/java/conf/SessionFactoryProvider.java
@@ -3,12 +3,14 @@ package conf;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import models.Course;
 import models.Student;
 
 public class SessionFactoryProvider {
 	public static SessionFactory provideSessionFactory() {
 		Configuration config = new Configuration();
 		config.addAnnotatedClass(Student.class);
+		config.addAnnotatedClass(Course.class);
 		return config.buildSessionFactory();
 	}
 }
