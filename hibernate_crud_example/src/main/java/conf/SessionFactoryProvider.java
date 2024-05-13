60bef5f16c9943524110ff8f39f942bb02afd2a0
Mugisha Regis <mugisharegis72@gmail.com>
2024-05-13 21:51:32 +0200

    update: add BoardingSchoolStudent and DaySchoolStudent annotated models

--- hibernate_crud_example/src/main/java/conf/SessionFactoryProvider.java
+++ hibernate_crud_example/src/main/java/conf/SessionFactoryProvider.java
@@ -3,7 +3,9 @@ package conf;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import models.BoardingSchoolStudent;
 import models.Course;
+import models.DaySchoolStudent;
 import models.Student;
 
 public class SessionFactoryProvider {
@@ -11,6 +13,8 @@ public class SessionFactoryProvider {
 		Configuration config = new Configuration();
 		config.addAnnotatedClass(Student.class);
 		config.addAnnotatedClass(Course.class);
+		config.addAnnotatedClass(DaySchoolStudent.class);
+		config.addAnnotatedClass(BoardingSchoolStudent.class);
 		return config.buildSessionFactory();
 	}
 }
